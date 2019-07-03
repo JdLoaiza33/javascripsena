f5ba9135ceacbe875aa051335e5f6e6948dfe29d
JdLoaiza <52002998+JdLoaiza33@users.noreply.github.com>
2019-07-03 11:35:58 -0500

    Add files via upload

---
+++ Nueva carpeta/sena.js
@@ -0,0 +1,79 @@
+//var nombre="juan"
+//var apellido= "loaiza"
+//alert("hola "+nombre+" "+apellido)
+/*var persona=[
+{
+	nombre:"juan",
+	edad: 18,
+	cedula: "1006857133",
+	peli_fav: "Sharck Boy y Lava Girl",
+	Gen_peli: "accion",
+},
+{
+	nombre:"andres",
+	edad: 17,
+	cedula: "1235465434",
+	peli_fav: "Que paso Ayer",
+	Gen_peli: "comedia",
+},
+{
+	nombre:"Jhonatan",
+	edad:16,
+	cedula:"1105983222",
+	peli_fav:"el paseo 4",
+	Gen_peli:"entretenimiento",
+}
+]
+for (var i =0 ; i < persona.length; i++)
+{
+	if (persona[i].edad>19 && persona[i].Gen_peli=="comedia") 
+	{
+		alert("a "+persona[i].nombre+" le gusta la pelicula "+persona[i].peli_fav+" y su genero favorito es "+persona[i].Gen_peli+" y se le recomienda ver estas peliculas: que paso ayer, se nos armo la gorda y no se aceptan devoluciones")
+	}
+	else if (persona[i].edad>17 && persona[i].Gen_peli=="accion")
+	{
+		alert("a "+persona[i].nombre+" le gusta la pelicula "+persona[i].peli_fav+" y su genero favorito es "+persona[i].Gen_peli+" y se le recomienda ver estas peliculas: Rapido y furioso, duro de matar y camino asia la muerte")
+	}
+    else if (persona[i].edad<=16 && persona[i].Gen_peli=="entretenimiento"){
+	alert("a "+persona[i].nombre+" le gusta la pelicula "+persona[i].peli_fav+" y su genero favorito es "+persona[i].Gen_peli+" y se le recomienda ver estas peliculas: se nos armo la gorda, el transportador y the 100")}else
+    {
+    alert("no tenemos peliculas para recomendar")}
+}*/
+//var cadena1= "carro bmw 2019"
+//var cadena2= "nuevo"
+/*console.log(cadena1.length)
+console.log(cadena1.charAt(0))
+console.log(cadena1.charAt(1))
+console.log(cadena1.charAt(2))
+console.log(cadena1.charAt(3))
+console.log(cadena1.charAt(4))*/
+/*console.log(cadena1.toLowerCase())
+console.log(cadena1.toUpperCase())*/
+//console.log(cadena1.concat(cadena2))
+//console.log(cadena1.endsWith("p"))
+//console.log(cadena1.includes("rr"))
+//console.log(cadena1.indexOf("bmw"))
+//console.log(cadena1.slice(0,7))
+//console.log(cadena1.split(" "))
+//console.log(cadena1.substring(4,7))
+//var cadena1= "carro"
+/*var array = ["Violin","Corno","Saxo","Guitarra","Bajo"]
+var arraynum = [343,5,6,9,566]*/
+//document.getElementById('ejercicioArray').innerHTML=array.toString()
+//document.getElementById('ejercicioArray').innerHTML=array.join()
+//document.getElementById('ejercicioArray').innerHTML=array.pop()
+//document.getElementById('ejercicioArray').innerHTML=array.push("flauta")
+//console.log(array)
+/*var txt = "APRENDIZ"
+var segundo= "sena"
+var txt2= txt.toLowerCase()
+document.getElementById('ejercicio').innerHTML=txt2.concat(" "+ segundo)
+console.log(txt)
+document.getElementById('apartar').innerHTML= txt.split('').join("+")
+document.getElementById('reversa').innerHTML= txt.split   
+reverse().join("")*/
+//document.getElementById('ejercicioArray').innerHTML=array.shift()
+//document.getElementById('ejercicioArray').innerHTML=array.unshift("Piano")
+//document.getElementById('ejercicioArray').innerHTML=array.sort()
+/*document.getElementById('ejercicioArray').innerHTML=array.sort() 
+console.log(array)*/
